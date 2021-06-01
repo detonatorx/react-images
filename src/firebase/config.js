5c8911e69382d976cf720137b595403b0a233e6f
tymchii.a <alexey.timchiy@sipuni.com>
2021-06-01 15:46:00 +0300

    upload image to storage, cteating link ref

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,20 +1,23 @@
-import * as firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/storage';
 import 'firebase/firestore';
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: process.env.REACT_API_KEY,
-  authDomain: process.env.REACT_AUTH_DOMAIN,
-  projectId: process.env.REACT_PROJECT_ID,
-  storageBucket: process.env.REACT_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID,
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_ID,
 };
+
+// console.log(firebaseConfig);
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export { projectStorage, projectFirestore };
+export { projectStorage, projectFirestore, timestamp };
--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,5 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { projectStorage } from '../firebase/config';
+import { useState, useEffect } from 'react';
+import {
+  projectStorage,
+  projectFirestore,
+  timestamp,
+} from '../firebase/config';
 
 const useStorage = file => {
   const [progress, setProgress] = useState(0);
@@ -8,6 +12,7 @@ const useStorage = file => {
 
   useEffect(() => {
     const storageRef = projectStorage.ref(file.name);
+    const collectionRef = projectFirestore.collection('images');
 
     storageRef.put(file).on(
       'state_changed',
@@ -20,6 +25,7 @@ const useStorage = file => {
       },
       async () => {
         const url = await storageRef.getDownloadURL();
+        collectionRef.add({ url, createdAt: timestamp() });
         setUrl(url);
       }
     );
